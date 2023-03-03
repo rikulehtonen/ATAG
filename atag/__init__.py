384e7f9974ba8f4971d82505c07834334659c903
Riku Lehtonen <contact@rikulehtonen.fi>
2023-03-03 12:19:40 +0200

    add config files

---
+++ atag/__init__.py
@@ -0,0 +1,25 @@
+
+from Browser import Browser
+from Browser.utils.data_types import SupportedBrowsers
+
+class ATAG_browser():
+    def __init__():
+        pass
+
+    def scanForActions():
+        pass
+
+    def returnActions():
+        pass
+
+
+
+#b = Browser(timeout="20 s", retry_assertions_for="500 ms")
+#b.new_browser(browser=SupportedBrowsers.chromium)
+#b.new_context(
+#    acceptDownloads=True,
+#    viewport={"width": 1920, "height": 1080}
+#)
+#b.new_page("https://playwright.dev")
+#assert b.get_text("h1") == "🎭 Playwright"
+#b.close_browser()
