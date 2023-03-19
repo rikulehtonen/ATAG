5cfe3b56cb362ce9fbaebc6c02c5e970621023e6
Riku Lehtonen <contact@rikulehtonen.fi>
2023-03-19 17:23:53 +0200

    Changes to Demo BrowserEnv

--- testing/test.py
+++ testing/test.py
@@ -12,6 +12,7 @@ b.new_page("file:///Users/riku/Documents/Aalto/ATAG/resources/login/login.html")
 b.click('xpath=//button[@id="loginBox"]')
 b.type_text('xpath=//input[@name="uname"]',"testaaja")
 b.type_text('xpath=//input[@name="psw"]',"testi")
+print(b.get_text('xpath=//input[@name="uname"]'))
 b.click('xpath=//button[@type="submit"]')
 assert b.get_text('xpath=//*[@id="logininfo"]') == 'Logged In'
 b.close_browser()
