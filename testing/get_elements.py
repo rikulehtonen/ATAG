11d88e205a6ffa67b8a2dc3586a42f568280bcc9
Riku Lehtonen <riku.lehtonen@tietoevry.com>
2023-04-10 00:53:48 +0300

    testing

--- testing/get_elements.py
+++ testing/get_elements.py
@@ -6,21 +6,33 @@ from Browser import AssertionOperator
 import multiprocessing
 import time
 
+from typing import TYPE_CHECKING, Any, Dict, Optional, Set, Union
+
+
+def checkpage(e):
+    #num = b.get_element_count('xpath=//form[@id="myForm"]')
+    e.get_element('xpath=//*[@id="loginBox"]')
+
 b = Browser(timeout="0 s", retry_assertions_for="0 ms")
 b.new_browser(headless=False, browser=SupportedBrowsers.chromium)
 b.new_context(
     acceptDownloads=True,
     viewport={"width": 700, "height": 500}
 )
-b.new_page('file://' + os.getcwd() + '/resources/login/login.html')
+#b.new_page('file://' + os.getcwd() + '/resources/login/login.html')
+b.new_page('https://fi.wikipedia.org/')
+time.sleep(2)
 
-def checkpage(b):
-    #num = b.get_element_count('xpath=//form[@id="myForm"]')
-    b.get_text('xpath=//*[@class="infobar"]') == 'Logged In'
+
+
+#e = b.get_element_count('xpath=//*')
+#print(e)
 
 start_time = time.time()
-for i in range(10000):
-    b.get_element_count('xpath=//form[@id="myForm"]')
+#ids = [b.get_property(elem,'id') for elem in e]
+
 print('done')
 print("--- %s seconds ---" % (time.time() - start_time))
+
+#print(ids)
 b.close_browser()
