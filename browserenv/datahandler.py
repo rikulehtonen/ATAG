aa01bb32416ae10e638dfb636311461aa833d42e
Riku Lehtonen <contact@rikulehtonen.fi>
2023-04-12 00:59:21 +0300

    update file names

---
+++ browserenv/datahandler.py
@@ -0,0 +1,29 @@
+
+
+class DataLoad:
+    def __init__(self):
+        self.folder = None
+
+    def load_elements(self):
+        pass
+
+    def load_actions(self):
+        pass
+
+    def load_targets(self):
+        pass
+
+
+
+class DataSave:
+    def __init__(self):
+        self.folder = None
+
+    def save_elements(self):
+        pass
+
+    def save_actions(self):
+        pass
+
+    def save_targets(self):
+        pass
