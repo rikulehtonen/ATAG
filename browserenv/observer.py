dd76578a6a047e9a474c975cfd5049ea39b825c0
Riku Lehtonen <riku.lehtonen@tietoevry.com>
2023-04-21 21:53:21 +0300

    add element capture

--- browserenv/observer.py
+++ browserenv/observer.py
@@ -14,11 +14,9 @@ class Observer:
         self.load = load
         self.save = save
 
-
     def reset(self):
         self.obsCount = 0
         self.done = False
-
 
     def __observeElements(self):
         ids = """Array.prototype.map.call(document.getElementsByTagName('*'), (element) => 
@@ -29,36 +27,37 @@ class Observer:
             } 
             else 
             { 
-                return {'tag': element.tagName, 'text': element.textContent, 'value': element.value, 'id': element.getAttribute('id'), 'name': element.getAttribute('name') } 
+                return {'tag': element.tagName, 'text': element.textContent, 'value': element.value, 'attributes': Array.prototype.map.call(element.attributes, (e) => ({ 'key': e.nodeName, 'value': e.nodeValue }) ) }
             } 
         }).filter(elements => { return elements !== null })"""
         scannedElements = self.browser.evaluate_javascript('xpath=//html', ids)
         elements = self.load.elements
-        
+
         if self.collectData:
             self.save.saveElements(scannedElements)
             self.save.saveActions(scannedElements)
         
         return np.array([1 if e in scannedElements else 0 for e in elements])
 
-
     def __observeTargets(self):
+        reward_sum = [0] 
         for target in self.load.targets:
             try:
                 getattr(self.b, target['keyword'])(*target['args'], **{})
-                return target['positive_reward']
+                reward_sum.append(target.get('positive_reward'))
+                self.done = target.get('is_done')
             except:
-                return target['negative_reward']
-
+                reward_sum.append(target.get('negative_reward'))
+            
+        reward_sum = sum(filter(None, reward_sum))
+        return reward_sum
 
     def __checkDone(self):
         if self.obsCount >= self.obsLimit:
             self.done = True
 
-
     def __checkReady(self):
         time.sleep(2)
-
 
     def observe(self):
         self.__checkReady()
