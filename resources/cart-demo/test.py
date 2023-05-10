dc533191b09aa1a6e4e6333faad932dedbb544d5
Riku Lehtonen <contact@rikulehtonen.fi>
2023-05-11 00:11:36 +0300

    add cart-demo

---
+++ resources/cart-demo/test.py
@@ -0,0 +1,14 @@
+from atag import Atag
+from browserenv import BrowserEnv
+
+
+def main():
+    browserEnv = BrowserEnv()
+    atag_browser = Atag(browserEnv, 0.01, 0.99)
+
+    atag_browser.test(1, 'results/model/episode_1800_params.pt')
+
+    browserEnv.terminate()
+
+if __name__ == '__main__':
+    main()
